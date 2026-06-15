9542e5d4d4b46dda848424a1e43e988d0288c236
shokichi0217 <moms.sho.y@gmail.com>
2026-06-16 07:58:22 +0900

    Web deployment: add friendly API error branching for unavailable/quota states

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2673253755" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2707480642" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
