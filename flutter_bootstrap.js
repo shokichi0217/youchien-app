4737baf0d4ecf85ab96a224e853cc060399c8082
shokichi0217 <moms.sho.y@gmail.com>
2026-06-25 08:53:18 +0900

    Update API error message for UX

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "59745365" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3477602937" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
