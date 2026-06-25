4bcb3914bb89d8a069a5d9d63bc12fd8df4b3e41
shokichi0217 <moms.sho.y@gmail.com>
2026-06-25 19:51:24 +0900

    Fix file_picker LateInitializationError on Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "61456099" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3872692872" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
